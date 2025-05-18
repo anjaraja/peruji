a6fafea2eee7c0c6556688443cf1a0edea9daa92
frjlnsyh <frjlnsyh@gmail.com>
2025-05-18 22:34:06 +0700

    perubahan text dan foto di home

--- resources/views/landingpage/home.blade.php
+++ resources/views/landingpage/home.blade.php
@@ -4,36 +4,82 @@
 
 @section('content')
     <div class="container-home position-relative z-0">
-        <div id="carouselHome" class="carousel slide h-100" data-bs-ride="carousel"  data-bs-pause="false" data-bs-interval="3000">
+        <div id="carouselHome" class="carousel slide h-100" data-bs-ride="carousel"  data-bs-pause="false" data-bs-interval="4000">
             <div class="carousel-inner h-100">
                 <div class="carousel-item active h-100" style="background-image: url({{asset('lp-img/Foto-Home-1.jpg')}});background-position: center;background-size: cover;">
                     {{-- <a href="#"></a> --}}
-                    <div class="carousel-text" style="position: absolute;top:0%;left:5%;z-index:2;">
-                        <object data="{{asset('lp-svg/Copy Home-1.svg')}}" type="image/svg+xml" width="1050"></object>
+                    <div class="carousel-text" style="position: absolute;top:-6%;left:5%;z-index:2;">
+                        <svg width="1150" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+                            <style type="text/css">
+                                .st0{fill:#f3f3f3; }
+                                .st1{font-family:'Helveticasss';font-weight: 100;}
+                                .st2{font-size:65.8918px;}
+                                .st3{fill: #f3f3f3;font-family:'Helveticass';font-weight: bolder}
+                            </style>
+                            <text transform="matrix(1 0 0 1 78.1001 333.9651)"><tspan x="0" y="0" class="st0 st1 st2">Empowering</tspan><tspan x="0" y="80" class="st1 st3 st2">Life Underwriters</tspan><tspan x="0" y="155" class="st0 st1 st2">to Safeguards the Future</tspan></text>
+                        </svg>
                     </div>
                 </div>
                 <div class="carousel-item h-100"style="background-image: url({{asset('lp-img/Foto-Home-2.jpg')}});background-position: center;background-size: cover;">
                     {{-- <a href="#"></a> --}}
-                    <div class="carousel-text" style="position: absolute;top:0%;left:5%;z-index:2;">
-                        <object data="{{asset('lp-svg/Copy Home-2.svg')}}" type="image/svg+xml" width="1050"></object>
+                    <div class="carousel-text" style="position: absolute;top:-6%;left:5%;z-index:2;">
+                        <svg width="1150"  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0{fill:#f3f3f3;}
+                            .st1{font-family:'Helveticasss';font-weight: 100;}
+                            .st2{font-size:65.8918px;}
+                            .st3{fill: #f3f3f3;font-family:'Helveticass';font-weight: bolder}
+                        </style>
+                        <text transform="matrix(1 0 0 1 78.1001 383.9998)"><tspan x="0" y="0" class="st0 st1 st2">Driving </tspan><tspan x="211.12" y="0" class="st0 st3 st2">Professionalism</tspan><tspan x="0" y="65" class="st0 st1 st2">in Risk Management</tspan></text>
+                        </svg>
                     </div>
                 </div>
                 <div class="carousel-item h-100"style="background-image: url({{asset('lp-img/Foto-Home-3.jpg')}});background-position: center;background-size: cover;">
                     {{-- <a href="#"></a> --}}
-                    <div class="carousel-text" style="position: absolute;top:0%;left:5%;z-index:2;">
-                        <object data="{{asset('lp-svg/Copy Home-3.svg')}}" type="image/svg+xml" width="1050"></object>
+                    <div class="carousel-text" style="position: absolute;top:-6%;left:5%;z-index:2;">
+                        <svg width="1150" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0{fill:#f3f3f3;}
+                            .st1{font-family:'Helveticasss';font-weight: 100;}
+                            .st2{font-size:65.8918px;}
+                            .st3{fill: #f3f3f3;font-family:'Helveticass';font-weight: bolder}
+                        </style>
+                        <text transform="matrix(1 0 0 1 78.1001 333.9651)"><tspan x="0" y="0" class="st0 st1 st2">Fostering</tspan><tspan x="0" y="65" class="st0 st3 st2">Growth and Standards</tspan><tspan x="0" y="130" class="st0 st1 st2">for Life Underwriters</tspan></text>
+                        </svg>
+
                     </div>
                 </div>
                 <div class="carousel-item h-100"style="background-image: url({{asset('lp-img/Foto-Home-4.jpg')}});background-position: center;background-size: cover;">
                     {{-- <a href="#"></a> --}}
-                    <div class="carousel-text" style="position: absolute;top:0%;left:5%;z-index:2;">
-                        <object data="{{asset('lp-svg/Copy Home-4.svg')}}" type="image/svg+xml" width="1050"></object>
+                    <div class="carousel-text" style="position: absolute;top:-6%;left:5%;z-index:2;">
+                        <svg width="1150" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0{fill:#f3f3f3;}
+                            .st1{font-family:'Helveticasss';font-weight: 100;}
+                            .st2{font-size:65.8918px;}
+                            .st3{fill: #f3f3f3;font-family:'Helveticass';font-weight: bolder}
+                        </style>
+                        <text transform="matrix(1 0 0 1 78.1001 333.9651)"><tspan x="0" y="0" class="st0 st1 st2">Building</tspan><tspan x="0" y="65" class="st0 st3 st2">a Strong and Ethical</tspan><tspan x="0" y="130" class="st0 st1 st2">Underwriter Community</tspan></text>
+                        </svg>
                     </div>
                 </div>
                 <div class="carousel-item h-100"style="background-image: url({{asset('lp-img/Foto-Home-5.jpg')}});background-position: center;background-size: cover;">
                     {{-- <a href="#"></a> --}}
-                    <div class="carousel-text" style="position: absolute;top:0%;left:5%;z-index:2;">
-                        <object data="{{asset('lp-svg/Copy Home-5.svg')}}" type="image/svg+xml" width="1050"></object>
+                    <div class="carousel-text" style="position: absolute;top:-6%;left:5%;z-index:2;">
+                        <svg width="1150" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                            viewBox="0 0 1366 768" style="enable-background:new 0 0 1366 768;" xml:space="preserve">
+                        <style type="text/css">
+                            .st0{fill:#f3f3f3;}
+                            .st1{font-family:'Helveticasss';font-weight: 100;}
+                            .st2{font-size:65.8918px;}
+                            .st3{fill: #f3f3f3;font-family:'Helveticass';font-weight: bolder}
+                        </style>
+                        <text transform="matrix(1 0 0 1 78.1001 333.9651)"><tspan x="0" y="0" class="st0 st1 st2">Championing</tspan><tspan x="0" y="65" class="st0 st3 st2">Innovation and Unity</tspan><tspan x="0" y="130" class="st0 st1 st2">in Life Underwriting</tspan></text>
+                        </svg>
                     </div>
                 </div>
             </div>
