64dc0a022d0400e158bdb9663c0d483d4e630765
anjaraja <anjarpratama16@gmail.com>
2025-05-19 00:15:30 +0700

    fixing RC1 on register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -60,7 +60,7 @@ class AuthController extends Controller
                 'access_token' => $token,
                 'user' => $user,
                 'token_type' => 'bearer',
-                'expires_in' => auth()->factory()->getTTL() * 60,
+                'expires_in' => auth("api")->factory()->getTTL() * 60,
             ]);
         }
         catch(\Exception $e){
