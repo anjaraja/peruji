a102b608ccc549bd93bde0609e6689c444e88db6
anjaraja <anjarpratama16@gmail.com>
2025-05-27 23:58:31 +0700

    update expire session jwt time to 1 day

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -151,7 +151,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => auth()->refresh(),
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'expires_in' => auth()->factory()->getTTL() * 1440
         ]);
     }
 }
