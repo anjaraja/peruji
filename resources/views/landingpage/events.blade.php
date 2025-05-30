ef66132454716422176c40c5989f60a9c498212b
anjaraja <anjarpratama16@gmail.com>
2025-05-30 09:45:03 +0700

    add link register in link register list event

--- resources/views/landingpage/events.blade.php
+++ resources/views/landingpage/events.blade.php
@@ -52,12 +52,12 @@
                                     </svg>
                                     DETAIL
                                 </span>
-                                <span class="register-event ml-auto" style="font-size: 14px;">
+                                <a href="{{route('eventsRegis', ['events' => $value['id']])}}" class="register-event ml-auto text-decoration-none text-white" style="font-size: 14px;">
                                     REGISTER
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
                                     </svg>
-                                </span>
+                                </a>
                             </div>
                             <div class="event-detail-container">
                                 <div class="container-fluid event-s">
