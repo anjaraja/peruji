fe2c5523622efd2a52e6ca54457d45a3cb24aadd
anjaraja <anjarpratama16@gmail.com>
2025-06-01 03:13:42 +0700

    alignment garis-1

--- resources/views/landingpage/app.blade.php
+++ resources/views/landingpage/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'PERUJI')</title>
     <link rel="stylesheet" href="{{ asset('lp-css/global.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/styless_6.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('lp-css/styless_7.css') }}"> 
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <script src="{{asset('dash-js/fetchhelper_1.js')}}"></script>
     <script src="{{asset('dash-js/jquery.js')}}"></script>
