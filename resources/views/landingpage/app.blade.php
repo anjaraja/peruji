30bb2c280330582050685e66a3ceff511fdf922f
frjlnsyh <frjlnsyh@gmail.com>
2025-06-07 19:46:00 +0700

    penyesuaian font

--- resources/views/landingpage/app.blade.php
+++ resources/views/landingpage/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'PERUJI')</title>
     <link rel="stylesheet" href="{{ asset('lp-css/global.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/navbar_8.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/styless_16.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('lp-css/navbar_9.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('lp-css/styless_17.css') }}"> 
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <script src="{{asset('dash-js/fetchhelper_2.js')}}"></script>
     <script src="{{asset('dash-js/jquery.js')}}"></script>
