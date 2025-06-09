6e44085434c18021ed4ed0650bd31e73a30eb311
frjlnsyh <frjlnsyh@gmail.com>
2025-06-10 00:14:14 +0700

    event & navbar

--- resources/views/landingpage/app.blade.php
+++ resources/views/landingpage/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'PERUJI')</title>
     <link rel="stylesheet" href="{{ asset('lp-css/global.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/navbar_9.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/styless_18.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('lp-css/navbar_10.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('lp-css/styless_19.css') }}"> 
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <script src="{{asset('dash-js/fetchhelper_2.js')}}"></script>
     <script src="{{asset('dash-js/jquery.js')}}"></script>
