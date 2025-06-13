9465b5b7c749a55c9d093bcf2b6e60d171ee0339
frjlnsyh <frjlnsyh@gmail.com>
2025-06-13 23:35:12 +0700

    perubahan list membership

--- resources/views/landingpage/membership.blade.php
+++ resources/views/landingpage/membership.blade.php
@@ -29,34 +29,37 @@
                     <div class="content gpe-l gpe-r">
                         <h3 class="m-0" lang="idn">Syarat & Ketentuan</h3>
                         <h3 class="m-0" lang="eng">Terms & Condition</h3>
-                        <ol class="m-0">
-                            <li lang="idn">
+                         <ol class="m-0" lang="idn">
+                            <li>
                                 Saat ini bekerja di Departemen Penjaminan Emisi dan/atau terlibat dalam proses Seleksi Risiko.
                             </li>
-                            <li lang="eng">
-                                Currently employed in an Underwriting Department and/or involved in the Risk Selection process.
-                            <li lang="idn">
+                            <li>
                                 Lengkapi formulir pendaftaran secara online.
                             </li>
-                            <li lang="eng">
-                                Complete the online registration form.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Bayar Biaya Pendaftaran
                             </li>
-                            <li lang="eng">
-                                Pay the registration fee.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Bayar Biaya Keanggotaan tahunan.
                             </li>
-                            <li lang="eng">
-                                Pay the annual membership fee.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Serahkan dokumen administratif yang diperlukan: bukti pembayaran, salinan tanda pengenal, surat dari pemberi kerja Anda saat ini, dan bukti kualifikasi atau sertifikasi profesional (jika tersedia).
                             </li>
-                            <li lang="eng">
+                        </ol>
+                        <ol class="m-0" lang="eng">
+                            <li>
+                                Currently employed in an Underwriting Department and/or involved in the Risk Selection process.
+                            </li>
+                            <li>
+                                Complete the online registration form.
+                            </li>
+                            <li>
+                                Pay the registration fee.
+                            </li>
+                            <li>
+                                Pay the annual membership fee.
+                            </li>
+                            <li>
                                 Submit the required administrative documents: proof of payment, a copy of your identification, a letter from your current employer, and proof of professional qualification or certification (if available).
                             </li>
                         </ol>
@@ -64,35 +67,37 @@
                     <div class="content gpe-l gpe-r">
                         <h3 class="m-0" lang="idn">Manfaat</h3>
                         <h3 class="m-0" lang="eng">Benefits</h3>
-                        <ol class="m-0">
-                            <li lang="idn">
+                        <ol class="m-0" lang="idn">
+                            <li>
                                 Kartu Keanggotaan.
                             </li>
-                            <li lang="eng">
-                                Membership Card.
-                            </li>
-                            <li lang="idn">
-                                Prioritaskan pada pendaftaran pelatihan/lokakarya/konferensi tingkat tinggi.
                             <li>
-                            <li lang="eng">
-                                Prioritize in training/workshop/summit registration.
+                                Prioritaskan pada pendaftaran pelatihan/lokakarya/konferensi tingkat tinggi.
                             </li>
-                            <li lang="idn">
+                            <li>
                                 Harga khusus untuk mitra Rumah Sakit/Laboratorium.
                             </li>
-                            <li lang="eng">
-                                Special price for Hospital / Laboratory partner.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Harga spesial Merchandise PERUJI.
                             </li>
-                            <li lang="eng">
-                                Special price for PERUJI Merchandise.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Harga khusus untuk kemitraan di masa mendatang.
                             </li>
-                            <li lang="eng">
+                        </ol>
+                        <ol class="m-0" lang="eng">
+                            <li>
+                                Membership Card.
+                            </li>
+                            <li>
+                                Prioritize in training/workshop/summit registration.
+                            </li>
+                            <li>
+                                Special price for Hospital / Laboratory partner.
+                            </li>
+                            <li>
+                                Special price for PERUJI Merchandise.
+                            </li>
+                            <li>
                                 Special price for future partnership.
                             </li>
                         </ol>
@@ -100,42 +105,46 @@
                     <div class="content gpe-l gpe-r">
                         <h3 class="m-0" lang="idn">How To Join</h3>
                         <h3 class="m-0" lang="eng">How To Join</h3>
-                        <ol class="m-0">
-                            <li lang="idn">
+                        <ol class="m-0" lang="idn">
+                            <li>
                                 Isi formulir pendaftaran melalui Google Forms (https://forms.gle/wDrBRnkoWqtNcD2P6) atau kirimkan versi cetak.
                             </li>
-                            <li lang="eng">
-                                Complete the registration form via Google Forms (https://forms.gle/wDrBRnkoWqtNcD2P6) or submit a hardcopy version.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Transfer biaya pendaftaran sebesar Rp100.000.
                             </li>
-                            <li lang="eng">
-                                Transfer the registration fee of Rp100,000.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Transfer biaya keanggotaan tahunan sebesar Rp300.000 (biaya ini akan dibebaskan jika Anda menghadiri Underwriting Summit tahunan).
                             </li>
-                            <li lang="eng">
-                                Transfer the annual membership fee of Rp300,000 (this fee will be waived if you attend the annual Underwriting Summit).
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Kirimkan dokumen yang dibutuhkan.
                             </li>
-                            <li lang="eng">
-                                Submit the required documents.
-                            </li>
-                            <li lang="idn">
+                            <li>
                                 Keanggotaan berlaku selama satu tahun dan dapat diperbarui.
                             </li>
-                            <li lang="eng">
+                        </ol>
+                        <ol class="m-0" lang="eng">
+                            <li>
+                                Complete the registration form via Google Forms ( <a href="https://forms.gle/wDrBRnkoWqtNcD2P6" style="background: transparent; color:blue;">https://forms.gle/wDrBRnkoWqtNcD2P6</a> ) or submit a hardcopy version.
+                            </li>
+                            <li>
+                                Transfer the registration fee of Rp100,000.
+                            </li>
+                            <li>
+                                Transfer the annual membership fee of Rp300,000 (this fee will be waived if you attend the annual Underwriting Summit).
+                            </li>
+                            <li>
+                                Submit the required documents.
+                            </li>
+                            <li>
                                 Membership is valid for one year and is renewable.
                             </li>
                         </ol>
                     </div>
                     <div class="content content-button gpe-l gpe-r">
                         <a href="{{route('membership-signup')}}" class="text-decoration-none px-4 py-2" id="button" lang="idn">Daftar</a>
-                        <a href="{{route('membership-signup')}}" class="text-decoration-none px-4 py-2" id="button" lang="eng">SIGN UP</a>
+                        <a href="{{route('membership-signup')}}" class="text-decoration-none px-4 py-2" id="button" lang="eng"
+                        style="max-width:120px; text-align: center;"
+                        >SIGN UP</a>
                     </div>
                 </div>
             </div>
