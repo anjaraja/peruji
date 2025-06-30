c6227a0be8f3a526e4e40ae392c4b0353d154d71
anjaraja <anjarpratama16@gmail.com>
2025-07-01 01:02:09 +0700

    update showing news links event

--- resources/views/landingpage/app.blade.php
+++ resources/views/landingpage/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('lp-css/global.css') }}"> 
     <link rel="stylesheet" href="{{ asset('lp-css/navbar_16.css') }}"> 
     <link rel="stylesheet" href="{{ asset('lp-css/events_5.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('lp-css/styless_35.css') }}">
+    <link rel="stylesheet" href="{{ asset('lp-css/styless_36.css') }}">
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <script src="{{asset('dash-js/fetchhelper_2.js')}}"></script>
     <script src="{{asset('dash-js/jquery.js')}}"></script>
