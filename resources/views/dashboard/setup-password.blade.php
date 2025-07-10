8925949dc8969e8f3081b49a43e668ca3ddc0ccc
anjaraja <anjarpratama16@gmail.com>
2025-07-10 09:30:53 +0700

    update setup password

--- resources/views/dashboard/setup-password.blade.php
+++ resources/views/dashboard/setup-password.blade.php
@@ -112,7 +112,7 @@
         </div>
     </main>
     @else
-    <div class="info-done position-absolute top-0 w-100 bg-white">
+    <div class="info-done position-absolute top-0 w-100 bg-white show">
         <div class="confetti-wrapper">
             <div class="content">
                 <h2>Congratulations! Your account verified and the password setup is complete.</h2>
@@ -122,6 +122,15 @@
         </div>
     </div>
     @endif
+    <div class="info-done position-absolute top-0 w-100 bg-white">
+        <div class="confetti-wrapper">
+            <div class="content">
+                <h2>Congratulations! Your account verified and the password setup is complete.</h2>
+                <h2>We will redirecting you to login page in 5 seconds.</h2>
+                <h2>If it's not automatically redirect, click this <a href="{{route('admin')}}">Link</a></h2>
+            </div>
+        </div>
+    </div>
 </body>
 <script src="{{asset('/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <script>
