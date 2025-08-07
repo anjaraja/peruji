e23575ceeab6798f0c5a62545412819e93fac0fc
anjaraja <anjarpratama16@gmail.com>
2025-08-07 21:33:51 +0700

    update uppercase title ecard

--- resources/views/dashboard/membership.blade.php
+++ resources/views/dashboard/membership.blade.php
@@ -499,7 +499,7 @@
         	card_container.querySelector(".profile-photo").style.background = `url("${responseData["photo"]}") center no-repeat`;
         	card_container.querySelector(".profile-photo").style.backgroundSize = "cover";
         	card_container.querySelector(".info-name").innerHTML = responseData["fullname"];
-        	card_container.querySelector(".info-details").innerHTML = responseData["title"];
+        	card_container.querySelector(".info-details").innerHTML =  responseData["title"]?.toUpperCase();
         	if(responseData["title"] == "Management"){
         		card_container.style.backgroundImage = `url("{{asset('dash-img/management-card.png')}}")`;
         	}
